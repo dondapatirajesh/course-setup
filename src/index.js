2195549111271f36325f3ed5edad4ec00e5a8885
dondapatirajesh <chowdaryrajesh1998@gmail.com>
2023-02-21 14:26:35 +0530

    use props concept

--- src/index.js
+++ src/index.js
@@ -1,9 +1,6 @@
 import ReactDOM from 'react-dom/client';
 
-import './index.css';
 import App from './App';
-import ExpenseItem from './components/ExpenseItem.js';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
-root.render(<App />);
-// root.render(<ExpenseItem />);
+root.render(<App/>);
