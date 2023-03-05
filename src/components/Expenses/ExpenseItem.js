d3c4aab79f79d54e98836ab9e9fee329ab731aa5
dondapatirajesh <chowdaryrajesh1998@gmail.com>
2023-03-06 01:02:56 +0530

    state-and-two-way-binding

--- src/components/Expenses/ExpenseItem.js
+++ src/components/Expenses/ExpenseItem.js
@@ -1,15 +1,34 @@
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
+import { useState } from 'react';
 
-function ExpenseItem(props) {
+const ExpenseItem = (props) => {
+
+  /* 
+  * Array destructuring
+  * @Param title contains the existing value.
+  * @Param setTitle contains the new value.
+  */
+  const [title, setTitle] = useState(props.title);
+  console.log('ExpenseItem evaluated by React');
+  
+  // setTitle() will call useState()
+  const clickHandler = () => {
+    setTitle("Updated!");
+    console.log(title);
+  }
+
   return (
     <Card className='expense-item'>
       <ExpenseDate date={props.date} />
       <div className='expense-item__description'>
-        <h2>{props.title}</h2>
+        <h2>{title}</h2>
         <div className='expense-item__price'>${props.amount}</div>
       </div>
+      
+      {/* clicking on this button invokes clickHandler function. */}
+      <button onClick={clickHandler}>Change Title</button>
     </Card>
   );
 }
